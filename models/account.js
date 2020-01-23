692c559c0e3a06b1fc0c9c545204691a8a63b384
Jarrod Chung <jarrodchung.dev@gmail.com>
2020-01-22 19:28:06 -0500

    Adding SQL table definitions to each model file

--- models/account.js
+++ models/account.js
@@ -1,3 +1,13 @@
+/**
+ * +-----------+--------------+------+-----+---------+----------------+
+ * | Field     | Type         | Null | Key | Default | Extra          |
+ * +-----------+--------------+------+-----+---------+----------------+
+ * | id        | int(11)      | NO   | PRI | NULL    | auto_increment |
+ * | name      | varchar(255) | YES  | UNI | NULL    |                |
+ * | createdAt | datetime     | NO   |     | NULL    |                |
+ * | updatedAt | datetime     | NO   |     | NULL    |                |
+ * +-----------+--------------+------+-----+---------+----------------+
+ */
 const Accounts = (sequelize, DataTypes) => {
   return sequelize.define("accounts", {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
--- models/category.js
+++ models/category.js
@@ -1,3 +1,14 @@
+/**
+ * +--------------+--------------+------+-----+---------+----------------+
+ * | Field        | Type         | Null | Key | Default | Extra          |
+ * +--------------+--------------+------+-----+---------+----------------+
+ * | id           | int(11)      | NO   | PRI | NULL    | auto_increment |
+ * | name         | varchar(255) | NO   | UNI | NULL    |                |
+ * | targetBudget | float        | NO   |     | 0       |                |
+ * | createdAt    | datetime     | NO   |     | NULL    |                |
+ * | updatedAt    | datetime     | NO   |     | NULL    |                |
+ * +--------------+--------------+------+-----+---------+----------------+
+ */
 const Categories = (sequelize, DataTypes) => {
   return sequelize.define("categories", {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
